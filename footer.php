e601f6748481ee936eb412a819e4a3155517897a
Kris <kristribbeck@gmail.com>
2022-11-21 12:28:28 +0800

    Deleted

--- footer.php
+++
@@ -1,9 +0,0 @@
-    <!--FOOTER START -->
-    <footer>
-        <div class="row">
-            <div class="col-md-6 ms-2">
-                <p>Copyright &copy; Rocky Rockets </p>
-            </div>
-        </div>
-    </footer>
-    <!--FOOTER END -->
